aef0423bbd813c7dbf9b9f379c7990314daf42a2
Jiaming Hu <horis233@gmail.com>
2020-11-23 10:16:46 -0500

    add kc link (#50)

--- controllers/namespacescope_controller.go
+++ controllers/namespacescope_controller.go
@@ -711,6 +711,7 @@ func (r *NamespaceScopeReconciler) getValidatedNamespaces(instance *operatorv1.N
 			validatedNs = append(validatedNs, nsMem)
 		} else {
 			klog.Infof("ibm-namespace-scope-operator doesn't have admin permission in namespace %s", nsMem)
+			klog.Infof("NOTE: Please refer to https://ibm.biz/cs_namespace_operator to authorize ibm-namespace-scope-operator permissions to namespace %s", nsMem)
 			r.Recorder.Eventf(instance, corev1.EventTypeWarning, "Forbidden", "ibm-namespace-scope-operator doesn't have admin permission in namespace %s", nsMem)
 		}
 	}

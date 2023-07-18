2c1d046beb35fea9855da8681a8073d854093009
Daniel Fan <fanyuchensx@gmail.com>
2023-07-18 13:18:08 -0400

    fix format issue

--- controllers/namespacescope_controller.go
+++ controllers/namespacescope_controller.go
@@ -1084,7 +1084,7 @@ func (r *NamespaceScopeReconciler) CSVReconcile(req ctrl.Request) (ctrl.Result, 
 		}
 		patchedCSVList = append(patchedCSVList, packageName.(string))
 
-		for _, c:= range csvList.Items {
+		for _, c := range csvList.Items {
 			// avoid Implicit memory aliasing in for loop
 			csv := c
 			klog.V(2).Infof("Found CSV %s for packageManifest %s", csv.Name, packageName.(string))

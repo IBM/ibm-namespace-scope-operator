fb76adb4cbe6a0fa1ce2daa1059e0720b6413bdb
YuChen Shen <59578388+YCShen1010@users.noreply.github.com>
2026-03-25 18:41:41 -0400

    Add liveness/readiness probes to operator deployment manifests  (#425)
    * Add liveness and readiness probes to the operator deployment
    
    Signed-off-by: Sophie Shen <syc_libra@live.cn>
    
    * Add liveness and readiness probes to helm
    
    Signed-off-by: Sophie Shen <syc_libra@live.cn>
    
    * Add health and readiness probes to the controller manager
    
    Signed-off-by: Sophie Shen <syc_libra@live.cn>
    
    * Add health and readiness probes to the operator deployment
    
    Signed-off-by: Sophie Shen <syc_libra@live.cn>
    
    * Add health check probes to operator helm
    
    Signed-off-by: Sophie Shen <syc_libra@live.cn>
    
    * Add liveness and readiness probes to csv
    
    Signed-off-by: Sophie Shen <syc_libra@live.cn>
    
    ---------
    
    Signed-off-by: Sophie Shen <syc_libra@live.cn>

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/klog"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	operatorv1 "github.com/IBM/ibm-namespace-scope-operator/v4/api/v1"
@@ -54,17 +55,20 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true))) // Use zap logger
 
 	var metricsAddr string
+	var probeAddr string
 	var enableLeaderElection bool
 	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-addr", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.Parse()
 
 	options := ctrl.Options{
-		Scheme:           scheme,
-		LeaderElection:   enableLeaderElection,
-		LeaderElectionID: "6a4a72f9.ibm.com",
+		Scheme:                 scheme,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "6a4a72f9.ibm.com",
 	}
 
 	operatorNs, err := common.GetOperatorNamespace()
@@ -91,6 +95,15 @@ func main() {
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		klog.Errorf("unable to set up health check: %v", err)
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		klog.Errorf("unable to set up ready check: %v", err)
+		os.Exit(1)
+	}
 
 	klog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
